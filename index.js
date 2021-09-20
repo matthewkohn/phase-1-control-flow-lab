f235181f0a526c3e9661072827a185d33921d615
Matthew <kohney319m@gmail.com>
2021-09-20 15:04:07 -0600

    commit tree 5b16c736e98728f14e0f68553595d2f3357a9d9b

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2500) {
+    return 'No can do.'; 
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(name){
   // Write your code here!
+  let city = name === 'NYC' ? 'Ok, sounds good.': 'No go.';
+  return city;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case tip = 'generous':
+      return 'Thank you so much.';
+      break;
+    case tip = 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
